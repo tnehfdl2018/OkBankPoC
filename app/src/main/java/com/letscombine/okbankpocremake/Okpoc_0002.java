78391ca9debd98279c07778dd2e8c2f36e008666
soobeenney <tnehfdl2011@gmail.com>
2021-01-25 13:33:02 +0900

    init commit

---
+++ app/src/main/java/com/letscombine/okbankpocremake/Okpoc_0002.java
@@ -0,0 +1,14 @@
+package com.letscombine.okbankpocremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Okpoc_0002 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okpoc_0002);
+    }
+}
---
+++ app/src/main/java/com/letscombine/okbankpocremake/Okpoc_0010_dialog.java
@@ -0,0 +1,13 @@
+package com.letscombine.okbankpocremake;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class Okpoc_0010_dialog extends Dialog {
+
+    public Okpoc_0010_dialog(@NonNull Context context) {
+        super(context);
+    }
+}
